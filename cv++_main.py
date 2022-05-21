867e8d82b7422db1c57946bbd9c767e838aa6bf7
Finn Lancaster <flancast90@gmail.com>
2022-05-20 22:10:08 -0400

    general code cleanup
    - make more strongly typed
    - change class to "clipboard"
    - remove unneeded global arguments
    - better variable names
    - fix infinite threading bug with thread creation in "while" loop
    - continuous listening class to not block main thread

--- cv++_main.py
+++ cv++_main.py
@@ -6,95 +6,62 @@ import time as t
 import tkinter as tk
 import threading as th
 
-# dict for caching all lines of text
-copy_dict = {1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '', 10: ''}
+class clipboard:
+    def __init__(self):
+        self.clipboard = {}
+        self.clipboard_length = 0
 
-# baseline for text and integers
-user_txt = ''
-user_num = 1
+    def add(self, value:str) -> dict:
+        self.clipboard[self.clipboard_length+1] = value
+        self.clipboard_length = self.clipboard_length+1
 
-# single class based for all worker functions, GUI separate
+        return self.clipboard
 
-class Worker: # grabs values needed for external use case
+    def clear(self) -> dict:
+        self.clipboard = {}
+        self.clipboard_length = 0
 
-    #
-    # copies text from dictionary to user's clipboard
-    # gets user input for which text to copy
-    # outputs text to user's clipboard
-    #
+        return self.clipboard
 
-    def __init__(self, copy_dict=copy_dict, user_num=user_num, user_txt=user_txt): # grabs dictionary values
-        self.copy_dict = copy_dict
-        print(self.copy_dict)
-        self.usr_num = user_num
-        print(self.usr_num)
-        self.user_txt = user_txt
-        print(self.user_txt)
-        self.out_txt = ''
+    def listen_continuously(self) -> None:
+        # to copy - CTRL+ALT+NUM
+        # compensate for held keys
+        held = {'copy': False,'paste': False}
 
+        while True:
+            if k.is_pressed(f'ctrl+c'):
+                if not held['copy']:
+                    # 0-9 (10 objects)
+                    if self.clipboard_length >= 10:
+                        self.clear()
 
-    def cache_copy(self): # copies user's clipboard to a dictionary value based on hotkey
-        if user_num != 0:
-            k.press_and_release('ctrl+c')
-            pyp.copy(self.user_txt)
-            pyp.paste()
-            self.copy_dict[self.usr_num] = self.user_txt
-            print('copied to cache')
-            copy_dict.update(self.copy_dict)
-            print('cache' + str(copy_dict))
-
-        else: # only for tenth copy or paste due to keyboard formatting irl
-            k.press_and_release('ctrl+c')
-            pyp.copy(self.user_txt)
-            pyp.paste()
-            self.copy_dict[10] = self.user_txt
-            print('copied to cache')
-            copy_dict.update(self.copy_dict)
-            print('cache' + str(copy_dict))
-            # confirm text cached
-
-
-    def cache_clear(self): # clears entire cache
-        self.copy_dict = {1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '', 10: ''}
-        copy_dict.update(self.copy_dict)
-        # clears cache
-
-    def listener_copy(self):
-        for i in range(1, 10):
-            print('run-copy')
-            if k.is_pressed(f'ctrl+alt+{str(i)}'): # uses previously compiled function to cache text selected
-                print('caching')
-                self.usr_num = i
-                self.user_txt = pyp.paste()
-                self.cache_copy(self)
-                print('cached')
-                print(copy_dict)
-                t.sleep(0.1)
-            
+                    self.add(pyp.paste())
+                held['copy'] = True
             else:
-                continue
+                held['copy'] = False
 
-    def listener_paste(self):
-        for i in range(1, 10):
-            print('run-paste')
-            if k.is_pressed(f'shift+ctrl+alt+{str(i)}'): # uses previously compiled function to paste text selected
-                print('pasting')
-                pyp.copy(str(copy_dict[i]))
-                k.press_and_release('ctrl+v')
-                t.sleep(0.1)
-                
-            else:
-                continue
+            # paste with ctrl+alt+v+{num}
+            # check from 1-9. 10 can't be typed as a single key
+            # so we use 0 and replace as 10.
+            for i in range(1,9):
+                i2 = 0 if i == 10 else i
+
+                if k.is_pressed(f'ctrl+alt+v+{str(i2)}'):
+                    if not held['paste']:
+                        pyp.copy(self.clipboard[i])
+                        pya.hotkey('ctrl', 'v')
+
+                        held['paste'] = True
+                    else:
+                        held['paste'] = False
+
+            t.sleep(0.01)
 
 
-# establish and run threads/main body
+if __name__ == "__main__":
+    # expandable listener function which calls
+    # copy/paste class based on keyboard
+    # action for more readability.
+    clipboard = clipboard()
 
-if __name__ == '__main__':
-
-    # establish __init__ passes for worker class
-    Worker.__init__(Worker, copy_dict)
-    print('worker init')
-
-    while 1:
-        th.Thread(target=Worker.listener_copy(Worker)).start()
-        th.Thread(target=Worker.listener_paste(Worker)).start()
+    th.Thread(target=clipboard.listen_continuously()).start()
